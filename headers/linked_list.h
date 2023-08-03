87aceb4e56d0b6505646ef73927bc9979258ffcc
WalterINF <walter.frank1059@gmail.com>
2023-08-03 11:26:42 -0300

    Adicionadas árvores trie e listas encadeadas

---
+++ headers/linked_list.h
@@ -0,0 +1,28 @@
+/*
+ Estrutura player_data:
+ name: String referente ao nome
+ id: valor correspondente ao sofifa_id
+ */
+typedef struct
+{
+    char *name;
+    int id;
+
+} player_data;
+/*
+ Estrutura listnode:
+ player: coleção de dados referentes ao jogador
+ next: ponteiro para próxima estrutura listnode
+ */
+struct listnode
+{
+    player_data player;
+    struct listnode* next;
+};
+struct listnode;
+typedef struct listnode listnode;
+
+listnode* initialize_list();
+void insert_list(listnode** list1, player_data player);
+void print_list(listnode *list1);
+void free_list(listnode *list1);
