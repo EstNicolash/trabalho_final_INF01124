5b8a39034e9c2818fa6fee34df6b3d4c56243381
Walter Frank <112022468+WalterINF@users.noreply.github.com>
2023-08-20 14:00:50 -0300

    Adicionado suporte para palavras com hífen, ponto e vírgula

--- headers/tag_trie.h
+++ headers/tag_trie.h
@@ -1,7 +1,7 @@
 #include "id_list.h"
 #include "misc.h"
 
-#define TRIE_ALPHABET_SIZE 27  // tamanho do vetor de descendentes para o nodo
+#define TRIE_ALPHABET_SIZE 30  // tamanho do vetor de descendentes para o nodo
 #define SPACE_INDEX 26
 #define LOWER_START 97
 #define LOWER_END 122
@@ -9,15 +9,15 @@
 #define UPPER_END 90
 /*
  Estrutura tag_trienode:
- value: caractere representado pelo nodo dentro da árvore
- list_of_ids: lista de id's aos quais foi atribuída a tag, serve como flag de nodo terminador,
-  sendo == NULL se não é terminador de palavra, caso contrário,
-  contém a lista de ids de jogadores que possuem aquela tag
-  last_of_list: ponteiro para o útlimo elemento da lista, para acelerar a inclusão de elementos
+ value: caractere representado pelo nodo dentro da Ã¡rvore
+ list_of_ids: lista de id's aos quais foi atribuÃ­da a tag, serve como flag de nodo terminador,
+  sendo == NULL se nÃ£o Ã© terminador de palavra, caso contrÃ¡rio,
+  contÃ©m a lista de ids de jogadores que possuem aquela tag
+  last_of_list: ponteiro para o Ãºtlimo elemento da lista, para acelerar a inclusÃ£o de elementos
 */
 struct tag_trienode {
     char value;
-    id_list *list_of_ids;  // NULL se n�o � terminador de palavra, caso contr�rio, cont�m a lista de ids de jogadores que possuem aquela tag
+    id_list *list_of_ids;  // NULL se não é terminador de palavra, caso contrário, contém a lista de ids de jogadores que possuem aquela tag
     struct tag_trienode *children[TRIE_ALPHABET_SIZE];  // vetor de descendentes
     id_list *last_of_list;
 };
@@ -25,7 +25,7 @@ struct tag_trienode;
 typedef struct tag_trienode tag_trienode;
 /*
  Estrutura tag_trienode:
- value: placeholder para alguma informação sobre a árvore
+ value: placeholder para alguma informaÃ§Ã£o sobre a Ã¡rvore
  root: ponteiro para a primeira estrutura tag_trienode
 */
 typedef struct tag_trie {
@@ -33,23 +33,11 @@ typedef struct tag_trie {
     tag_trienode *root;
 } TAG_TRIE;
 
-void initialize_tag_trie(TAG_TRIE *tree);  // inicializa a �rvore trie
 void initialize_tag_trie(TAG_TRIE *tree);  // inicializa a árvore trie
+void initialize_tag_trie(TAG_TRIE *tree);  // inicializa a Ã¡rvore trie
 
 tag_trienode *initialize_tag_trienode(char value);  // aloca, inicializa e retorna um ponteiro para um novo nodo
 
-uint getIndexFromChar(char char_value);  //mapeia um caractere para um indice no vetor, que vai de 0 at� 26
-
-void print_tag_trienode(tag_trienode *node1);  // printa o valor do nodo seguido de seus descendentes,
-                                               // indicando se cada um existe (1) ou n�o (0)
-
-void insert_tag_trie(TAG_TRIE tree, UserTag new_tag);  // insere uma palavra na �rvore trie
-
-int hasTagChild(tag_trienode *node1,
-                char char_value);  // verifica se um nodo da �rvore tem a letra como descendente
-
-int isTagPresent(TAG_TRIE tree,
-                 char name[]);           // verifica se uma palavra est� dentro detro da �rvore trie
 uint getIndexFromChar(char char_value);  //mapeia um caractere para um indice no vetor, que vai de 0 até 26
 
 void print_tag_trienode(tag_trienode *node1);  // printa o valor do nodo seguido de seus descendentes,
@@ -61,27 +49,39 @@ int hasTagChild(tag_trienode *node1,
                 char char_value);  // verifica se um nodo da árvore tem a letra como descendente
 
 int isTagPresent(TAG_TRIE tree,
-                 char name[]);  // verifica se uma palavra está dentro detro da árvore trie
+                 char name[]);           // verifica se uma palavra está dentro detro da árvore trie
+uint getIndexFromChar(char char_value);  //mapeia um caractere para um indice no vetor, que vai de 0 atÃ© 26
 
-char getCharFromTag(char string[],
-                    int index);  // retorna o caractere presente no �ndice dado dentro da palavra
+void print_tag_trienode(tag_trienode *node1);  // printa o valor do nodo seguido de seus descendentes,
+                                               // indicando se cada um existe (1) ou nÃ£o (0)
 
-id_list *list_all_ids(TAG_TRIE tree, char *tag_text);
-//retorna uma lista com todos os id's que tem a tag pesquisada
+void insert_tag_trie(TAG_TRIE tree, UserTag new_tag);  // insere uma palavra na Ã¡rvore trie
+
+int hasTagChild(tag_trienode *node1,
+                char char_value);  // verifica se um nodo da Ã¡rvore tem a letra como descendente
+
+int isTagPresent(TAG_TRIE tree,
+                 char name[]);  // verifica se uma palavra estÃ¡ dentro detro da Ã¡rvore trie
+
 char getCharFromTag(char string[],
                     int index);  // retorna o caractere presente no índice dado dentro da palavra
 
 id_list *list_all_ids(TAG_TRIE tree, char *tag_text);
 //retorna uma lista com todos os id's que tem a tag pesquisada
+char getCharFromTag(char string[],
+                    int index);  // retorna o caractere presente no Ã­ndice dado dentro da palavra
+
+id_list *list_all_ids(TAG_TRIE tree, char *tag_text);
+//retorna uma lista com todos os id's que tem a tag pesquisada
 
 id_list *intersection(id_list *list1, id_list *list2);
-//retorna uma lista de id's jogadores contendo apenas os id's que estão nas duas listas simulateamente
+//retorna uma lista de id's jogadores contendo apenas os id's que estÃ£o nas duas listas simulateamente
 
 id_list *intersection_multiple(TAG_TRIE tree, char *tags[], int num_tags);
 //recebe um vetor de tags e retorna os id's dos jogadores que possuem todas as tags simultaneamente
 
 id_list *intersection(id_list *list1, id_list *list2);
-//retorna uma lista de id's jogadores contendo apenas os id's que est�o nas duas listas simulateamente
+//retorna uma lista de id's jogadores contendo apenas os id's que estão nas duas listas simulateamente
 
 id_list *intersection_multiple(TAG_TRIE tree, char *tags[], int num_tags);
 //recebe um vetor de tags e retorna os id's dos jogadores que possuem todas as tags simultaneamente
