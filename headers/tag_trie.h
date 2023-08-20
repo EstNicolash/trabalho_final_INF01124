b89b083f8ccdd475535ec42a9301b47c1722f529
Walter Frank <112022468+WalterINF@users.noreply.github.com>
2023-08-20 12:23:45 -0300

    Otimização na inserção de IDs

--- headers/tag_trie.h
+++ headers/tag_trie.h
@@ -9,21 +9,23 @@
 #define UPPER_END 90
 /*
  Estrutura tag_trienode:
- value: caractere representado pelo nodo dentro da ·rvore
- id_list: lista de id's aos quais foi atribuÌda a tag, serve como flag de nodo terminador,
-  sendo == NULL se n„o È terminador de palavra, caso contr·rio,
-  contÈm a lista de ids de jogadores que possuem aquela tag
+ value: caractere representado pelo nodo dentro da √°rvore
+ list_of_ids: lista de id's aos quais foi atribu√≠da a tag, serve como flag de nodo terminador,
+  sendo == NULL se n√£o √© terminador de palavra, caso contr√°rio,
+  cont√©m a lista de ids de jogadores que possuem aquela tag
+  last_of_list: ponteiro para o √∫tlimo elemento da lista, para acelerar a inclus√£o de elementos
 */
 struct tag_trienode {
   char value;
-    id_list *list_of_ids; // NULL se n„o È terminador de palavra, caso contr·rio, contÈm a lista de ids de jogadores que possuem aquela tag
+    id_list *list_of_ids; // NULL se n√£o √© terminador de palavra, caso contr√°rio, cont√©m a lista de ids de jogadores que possuem aquela tag
+    id_list *last_of_list; 
   struct tag_trienode *children[TRIE_ALPHABET_SIZE]; // vetor de descendentes
 };
 struct tag_trienode;
 typedef struct tag_trienode tag_trienode;
 /*
  Estrutura tag_trienode:
- value: placeholder para alguma informaÁ„o sobre a ·rvore
+ value: placeholder para alguma informa√ß√£o sobre a √°rvore
  root: ponteiro para a primeira estrutura tag_trienode
 */
 typedef struct tag_trie {
@@ -31,31 +33,31 @@ typedef struct tag_trie {
   tag_trienode *root;
 } TAG_TRIE;
 
-void initialize_tag_trie(TAG_TRIE *tree); // inicializa a ·rvore trie
+void initialize_tag_trie(TAG_TRIE *tree); // inicializa a √°rvore trie
 
 tag_trienode *initialize_tag_trienode(
     char value); // aloca, inicializa e retorna um ponteiro para um novo nodo
 
-uint getIndexFromChar(char char_value); //mapeia um caractere para um indice no vetor, que vai de 0 atÈ 26
+uint getIndexFromChar(char char_value); //mapeia um caractere para um indice no vetor, que vai de 0 at√© 26
 
 void print_tag_trienode(
     tag_trienode *node1); // printa o valor do nodo seguido de seus descendentes,
-                      // indicando se cada um existe (1) ou n„o (0)
+                      // indicando se cada um existe (1) ou n√£o (0)
 
-void insert_tag_trie(TAG_TRIE tree, UserTag new_tag); // insere uma palavra na ·rvore trie
+void insert_tag_trie(TAG_TRIE tree, UserTag new_tag); // insere uma palavra na √°rvore trie
 
 int hasTagChild(
     tag_trienode *node1,
-    char char_value); // verifica se um nodo da ·rvore tem a letra como descendente
+    char char_value); // verifica se um nodo da √°rvore tem a letra como descendente
 
 int isTagPresent(
     TAG_TRIE tree,
-    char name[]); // verifica se uma palavra est· dentro detro da ·rvore trie
+    char name[]); // verifica se uma palavra est√° dentro detro da √°rvore trie
 
 
 char getCharFromTag(
     char string[],
-    int index); // retorna o caractere presente no Ìndice dado dentro da palavra
+    int index); // retorna o caractere presente no √≠ndice dado dentro da palavra
 
 id_list *list_all_ids(TAG_TRIE tree,
                       char *tag_text);
@@ -63,7 +65,7 @@ id_list *list_all_ids(TAG_TRIE tree,
 
 id_list *intersection(id_list *list1,
                       id_list *list2);
-                      //retorna uma lista de id's jogadores contendo apenas os id's que est„o nas duas listas simulateamente
+                      //retorna uma lista de id's jogadores contendo apenas os id's que est√£o nas duas listas simulateamente
 
 id_list *intersection_multiple(TAG_TRIE tree,
                                 char *tags[],
